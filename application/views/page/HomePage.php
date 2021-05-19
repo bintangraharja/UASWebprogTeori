97c30161bcf313308d535e0fad11f24e92e70d5b
AnandaSetiyani <ananda.setiyani@student.umn.ac.id>
2021-05-19 22:30:05 +0700

    Nan try 8

--- application/views/page/HomePage.php
+++ application/views/page/HomePage.php
@@ -15,41 +15,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<?php echo $sidebar; ?>
 			<h1 class="headTitle">Our Console</h1>
 		<br><br><br>
-		<?php 
-		foreach($product as $data){
-			$consolID = $data['ConsoleID'];
-			$consolName = $data['ConsoleName'];
-			$i = 0;
-			
-			if($i == 3){
-				echo "<div class='row'>";
-			}
-		?>
-		<div class="col-sm-4 pad">
-			<a href="<?php echo site_url('detail').'/'.$consolID;?>" class="menu-border">
-				<div class="card mid" style="width: 85%;">
-				<img class="card-img-top" style="width: 100%" src="<?php echo site_url('home/showImg/').$consolID ?>">
-					<div class="card-body">
-						<h4 class="card-title mid"><?php echo $consolName?></h4>
-						<br><br>
-						<footer class="blockquote-footer seeDetails">
-							<small>
-								See Details
-							</small>
-						</footer>
+		<div class="row">
+			<?php 
+			foreach($product as $data){
+				$consolID = $data['ConsoleID'];
+				$consolName = $data['ConsoleName'];
+				$i = 0;
+				
+				if($i == 3){
+					echo "<div class='row'>";
+				}
+			?>
+			<div class="col-sm-4 pad">
+				<a href="<?php echo site_url('detail').'/'.$consolID;?>" class="menu-border">
+					<div class="card mid">
+						<img class="card-img-top" style="width: 100%" src="<?php echo site_url('home/showImg/').$consolID ?>">
+						<div class="card-body">
+							<h4 class="card-title mid"><?php echo $consolName?></h4>
+							<br><br>
+							<footer class="blockquote-footer seeDetails">
+								<small>
+									See Details
+								</small>
+							</footer>
+						</div>
 					</div>
-				</div>
-			</a>
+				</a>
+			</div>
+			<?php
+				if($i == 3){
+					echo "</div>";
+					$i = 0;
+				}
+				$i++;
+				}
+			?>
 		</div>
-		<?php
-		if($i == 3){
-			echo "</div>";
-			$i = 0;
-		}
-		$i++;
-		
-		}
-		?>
 		<br><br>
 	</div>
 </body>
